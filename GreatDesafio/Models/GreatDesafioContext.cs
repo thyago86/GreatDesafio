40ecb2b964e76ca9e8557391afd06a278ff8a4df
Thyago Bezerra Lima <thyagobezerra8608@gmail.com>
2020-07-27 23:42:57 -0300

    Enviando desafio

---
+++ GreatDesafio/Models/GreatDesafioContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace GreatDesafio.Models
+{
+    public partial class GreatDesafioContext : DbContext
+    {
+        public GreatDesafioContext()
+        {
+        }
+
+        public GreatDesafioContext(DbContextOptions<GreatDesafioContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-L45D8MT\\SQLEXPRESS2019;Initial Catalog=GreatDesafio;Persist Security Info=False;User ID=great;Password=q1w2e3r4t5;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.Property(e => e.Cpf)
+                    .IsRequired()
+                    .HasMaxLength(14)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.DataCadastro).HasColumnType("datetime");
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomeMae)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomePai)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Rg)
+                    .IsRequired()
+                    .HasColumnName("RG")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ GreatDesafio/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GreatDesafio.Models
+{
+    public partial class Usuario
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string NomeMae { get; set; }
+        public string NomePai { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public int Id { get; set; }
+    }
+}
